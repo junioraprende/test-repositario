179346074de4420dd8101bf050be87a31cd46aeb
Junior Juarez <junmariodesk@outlook.com>
2022-09-11 00:13:25 -0600

    vista escuela

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using HolaMundoMVC.Models;
+namespace HolaMundoMVC
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var escuela = new Escuela();
+            escuela.AñoFundación = 1995;
+            escuela.EscuelaId = Guid.NewGuid().ToString();
+            escuela.Nombre = "Platzi School";
+
+
+
+            return View(escuela);
+            //c# ya sabe por convencion que me interesa regresar la vista de Index
+        }
+    }
+}
