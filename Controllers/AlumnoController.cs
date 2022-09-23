1d3f4c0353b2c8fd2e727e997dad0ad03f0ffaeb
Junior Juarez <junmariodesk@outlook.com>
2022-09-23 16:24:30 -0600

    Fin de tabla

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -31,13 +31,17 @@ namespace HolaMundoMVC
         {
 
             string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes" };
             string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
-            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
 
             var listaAlumnos = from n1 in nombre1
                                from n2 in nombre2
                                from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}" };
+                               select new Alumno
+                               {
+                                   Nombre = $"{n1} {n2} {a1}",
+                                   UniqueId = Guid.NewGuid().ToString()
+                               };
 
             return listaAlumnos.OrderBy((al) => al.UniqueId).ToList();
         }
